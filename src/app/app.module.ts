6f6d7a689843d79e758d4a76b79ebf6ed289e82a
HUGO-BENDER <hbender@absis.com>
2020-11-04 13:16:18 +0100

    Step 007 b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './components/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHomeComponent } from './components/page-home/page-home.component';
+import { PageAboutComponent } from './components/page-about/page-about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PagePolicyPrivacyComponent } from './components/page-policy-privacy/page-policy-privacy.component';
+import { PageServiceConditionsComponent } from './components/page-service-conditions/page-service-conditions.component';
+import { AppLogoComponent } from './shared-components/app-logo/app-logo.component';
+import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
+import { RefactoryComponent } from './games/refactory/refactory/refactory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHomeComponent,
+    PageAboutComponent,
+    PageNotFoundComponent,
+    PagePolicyPrivacyComponent,
+    PageServiceConditionsComponent,
+    AppLogoComponent,
+    AppToolbarComponent,
+    RefactoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/games/refactory/refactory/refactory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'game-refactory',
+  templateUrl: './refactory.component.html',
+  styleUrls: ['./refactory.component.scss']
+})
+export class RefactoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
